87c4c3b57b7424f68a5b002f5f55eeef88bf8230
gpm777 <andrewsseven@gmail.com>
2018-05-30 17:54:24 -0300

    Novos testes de carga de documentos NCL.

---
+++ API(NCube)/test/createDocument/CreateTest6(A).lua
@@ -0,0 +1,43 @@
+require("core/NCube")
+
+local doc = Document:create({id="exemplo06ConnBase",
+  xmlns="http://www.ncl.org.br/NCL3.0/EDTVProfile"},
+"<?xml version= \"1.0\" encoding=\"ISO-8859-1\"?>")
+
+local head = Head:create()
+
+local connectorBase = ConnectorBase:create()
+
+local onBeginStart = CausalConnector:create({id = "onBeginStart"})
+local onBegin = SimpleCondition:create({role = "onBegin"})
+local start = SimpleAction:create({role = "start", max="unbounded", qualifier="par"})
+onBeginStart:setSimpleCondition(onBegin)
+onBeginStart:setSimpleAction(start)
+connectorBase:addCausalConnector(onBeginStart)
+
+local onEndStart = CausalConnector:create({id = "onEndStart"})
+local onEnd = SimpleCondition:create({role = "onEnd"})
+onEndStart:setSimpleCondition(onEnd)
+onEndStart:setSimpleAction(start)
+connectorBase:addCausalConnector(onEndStart)
+
+local onKeySelectionStartStopAbort =
+  CausalConnector:create({id = "onKeySelectionStartStopAbort"})
+local keyCode = ConnectorParam:create({name = "keyCode"})
+local onSelection = SimpleCondition:create({role="onSelection", key="$keyCode"})
+local seq = CompoundAction:create({operator="seq"})
+seq:addSimpleAction(SimpleAction:create({role = "start"}))
+seq:addSimpleAction(SimpleAction:create({role = "stop"}))
+seq:addSimpleAction(SimpleAction:create({role = "abort"}))
+onKeySelectionStartStopAbort:addConnectorParam(keyCode)
+onKeySelectionStartStopAbort:setSimpleCondition(onSelection)
+onKeySelectionStartStopAbort:setCompoundAction(seq)
+connectorBase:addCausalConnector(onKeySelectionStartStopAbort)
+
+head:setConnectorBase(connectorBase)
+
+doc:setHead(head)
+
+local fileName = "test/createDocument/docs/connectorBase6.conn"
+doc:saveNcl(fileName)
+os.execute("java -jar ncl-validator-1.4.20.jar -nl pt_BR "..fileName)
