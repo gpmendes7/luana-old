612a48142559f178c405dcd2c088de6422acd44d
gpm777 <andrewsseven@gmail.com>
2018-06-06 11:26:43 -0300

    Documentação das classes SimpleCondition e ValueAssessment.

--- API(NCube)/src/core/connectors/ValueAssessment.lua
+++ API(NCube)/src/core/connectors/ValueAssessment.lua
@@ -1,13 +1,47 @@
 local NCLElem = require "core/NCLElem"
 
+---
+-- Implements ValueAssessment Class representing <b>&lt;valueAssessment&gt;</b> element.
+-- 
+-- Implemented based on: <a href="http://handbook.ncl.org.br/doku.php?id=valueassessment">
+-- http://handbook.ncl.org.br/doku.php?id=valueassessment</a>
+-- 
+-- @module ValueAssessment
+-- 
+-- @extends #NCLElement
+-- 
+-- @author Gabriel Pereira Mendes
+-- 
+-- @usage 
+-- -- The module needs to be imported to be used with the instruction
+-- local ValueAssessment = require "core/connectors/ValueAssessment" 
 local ValueAssessment = NCLElem:extends()
 
+---
+-- Name of <b>&lt;valueAssessment&gt;</b> element.
+-- 
+-- @field [parent=#ValueAssessment] #string nameElem
 ValueAssessment.nameElem = "valueAssessment"
 
+---
+-- List containing the data types of each attribute
+-- belonging to <b>&lt;valueAssessment&gt;</b> element.
+-- 
+-- @field [parent=#ValueAssessment] #table attributesTypeMap 
 ValueAssessment.attributesTypeMap = {
   value = "string"
 }
 
+---
+-- Returns a new ValueAssessment object. 
+-- If `full` flag is not nil, the object will
+-- receive default children objects of each children class.
+-- 
+-- This case, `full` must be passed to the method with a valid number.  
+-- 
+-- @function [parent=#ValueAssessment] create
+-- @param #table attributes list of attributes to be initialized.
+-- @return #ValueAssessment new ValueAssessment object created.
 function ValueAssessment:create(attributes)
   local valueAssessment = ValueAssessment:new()
 
@@ -20,10 +54,23 @@ function ValueAssessment:create(attributes)
   return valueAssessment
 end
 
+---
+-- Sets a value to `value` attribute of the 
+-- <b>&lt;valueAssessment&gt;</b> element. 
+-- 
+-- @function [parent=#ValueAssessment] setValue
+-- @param #string value `value` atribute of the
+-- <b>&lt;valueAssessment&gt;</b> element.
 function ValueAssessment:setValue(value)
   self:addAttribute("value", value)
 end
 
+---
+-- Returns the value of the `value` attribute of the 
+-- <b>&lt;valueAssessment&gt;</b> element. 
+-- 
+-- @function [parent=#ValueAssessment] getValue
+-- @return #string `value` atribute of the <b>&lt;valueAssessment&gt;</b> element.
 function ValueAssessment:getValue()
   return self:getAttribute("value")
 end
