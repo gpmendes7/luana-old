8b33f1c8914f171e548aff66397dcf8cd41c4b08
gpm777 <andrewsseven@gmail.com>
2018-06-12 09:13:32 -0300

    Documentação da classe Property.

--- LuaNa(API)/src/core/interface/Property.lua
+++ LuaNa(API)/src/core/interface/Property.lua
@@ -1,15 +1,45 @@
 local NCLElem = require "core/NCLElem"
 
+---
+-- Implements Property Class representing <b>&lt;property&gt;</b> element.
+-- 
+-- Implemented based on: <a href="http://handbook.ncl.org.br/doku.php?id=property">
+-- http://handbook.ncl.org.br/doku.php?id=property</a>
+-- 
+-- @module Property
+-- 
+-- @extends #NCLElement
+-- 
+-- @author Gabriel Pereira Mendes
+-- 
+-- @usage 
+-- -- The module needs to be imported to be used with the instruction
+-- local Property = require "core/interface/Property" 
 local Property = NCLElem:extends()
 
+---
+-- Name of <b>&lt;property&gt;</b> element.
+-- 
+-- @field [parent=#Property] #string nameElem
 Property.nameElem = "property"
 
+---
+-- List containing the data types of each attribute
+-- belonging to <b>&lt;property&gt;</b> element.
+-- 
+-- @field [parent=#Property] #table attributesTypeMap 
 Property.attributesTypeMap = {
   name = "string",
   value = {"string", "number"},
   externable = "boolean"
 }
 
+---
+-- Returns a new Property object. 
+-- 
+-- @function [parent=#Property] create
+-- @param #table attributes list of attributes to be initialized.
+-- @return #Property new Property object created.
 function Property:create(attributes)
   local property = Property:new()
 
@@ -26,26 +56,65 @@ function Property:create(attributes)
   return property
 end
 
+---
+-- Sets a value to `name` attribute of the 
+-- <b>&lt;property&gt;</b> element. 
+-- 
+-- @function [parent=#Property] setName
+-- @param #string name `name` atribute of the
+-- <b>&lt;property&gt;</b> element.
 function Property:setName(name)
   self:addAttribute("name", name)
 end
 
+---
+-- Returns the value of the `name` attribute of the 
+-- <b>&lt;property&gt;</b> element. 
+-- 
+-- @function [parent=#Property] getName
+-- @return #string `name` atribute of the <b>&lt;property&gt;</b> element.
 function Property:getName()
   return self:getAttribute("name")
 end
 
+---
+-- Sets a value to `value` attribute of the 
+-- <b>&lt;property&gt;</b> element. 
+-- 
+-- @function [parent=#Property] setValue
+-- @param #stringOrnumber value `value` atribute of the
+-- <b>&lt;property&gt;</b> element.
 function Property:setValue(value)
   self:addAttribute("value", value)
 end
 
+---
+-- Returns the value of the `value` attribute of the 
+-- <b>&lt;property&gt;</b> element. 
+-- 
+-- @function [parent=#Property] getValue
+-- @return #stringOrnumber `value` atribute of the <b>&lt;property&gt;</b> element.
 function Property:getValue()
   return self:getAttribute("value")
 end
 
+---
+-- Sets a value to `externable` attribute of the 
+-- <b>&lt;property&gt;</b> element. 
+-- 
+-- @function [parent=#Property] setExternable
+-- @param #boolean externable `externable` atribute of the
+-- <b>&lt;property&gt;</b> element.
 function Property:setExternable(externable)
   self:addAttribute("externable", externable)
 end
 
+---
+-- Returns the value of the `externable` attribute of the 
+-- <b>&lt;property&gt;</b> element. 
+-- 
+-- @function [parent=#Property] getExternable
+-- @return #boolean `externable` atribute of the <b>&lt;property&gt;</b> element.
 function Property:getExternable()
   return self:getAttribute("externable")
 end
