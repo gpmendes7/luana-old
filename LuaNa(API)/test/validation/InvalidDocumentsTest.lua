dd5224c4a782b23029e87d8dc9c3ee960facb729
gabriel7mendes <andrewsseven@gmail.com>
2023-03-03 19:33:19 -0300

    creates scripts for test some of validations

---
+++ LuaNa(API)/test/validation/InvalidDocumentsTest.lua
@@ -0,0 +1,21 @@
+require("LuaNa(API)/src/core/LuaNa")
+
+-- local doc1 = Document:create()
+-- doc1:loadNcl("LuaNa(API)/docs/invaliddocuments/invalidDocument1.ncl")
+
+-- Error! Document with invalid syntax!
+-- local doc2 = Document:create()
+-- doc2:loadNcl("LuaNa(API)/docs/invaliddocuments/invalidDocument2.ncl")
+
+-- Error! Unknown element with id rgVideo2 associated to descriptor element!
+--local doc3 = Document:create()
+-- doc3:loadNcl("LuaNa(API)/docs/invaliddocuments/invalidDocument3.ncl")
+ 
+ -- Error! Document with invalid syntax!
+ -- local doc4 = Document:create()
+ -- doc4:loadNcl("LuaNa(API)/docs/invaliddocuments/invalidDocument4.ncl")
+ 
+-- Error! Document with invalid syntax!
+-- local doc5 = Document:create()
+-- doc5:loadNcl("LuaNa(API)/docs/invaliddocuments/invalidDocument5.ncl")
+
